1af19967c74c3664dedfd1994e0c2cdb79468d9e
Дмитрий Гавриш <demetriusgarson@gmail.com>
2026-07-02 16:47:12 +0300

    base structure

--- src/main.js
+++ src/main.js
@@ -2,4 +2,8 @@ import './js/header.js';
 import './js/hero.js';
 import './js/event-list.js';
 import './js/about-us.js';
+import './js/faq.js';
 import './js/feedback.js';
+import './js/footer.js';
+import './js/event-details-modal.js';
+import './js/booking-modal.js';
