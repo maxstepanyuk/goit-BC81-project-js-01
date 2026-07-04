acac9c206e95756dd846ab614a399359068a0ed8
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-07-05 00:38:59 +0300

    feedback - swipper - add js

--- src/js/feedback.js
+++ src/js/feedback.js
@@ -1,1 +1,23 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation'; // todo: are too hard to override
 
+const feedbackSwiperElement = document.querySelector('.feedback-swiper');
+
+const swiperOptions = {
+  spaceBetween: 32,
+
+  pagination: {
+    el: '.swiper-pagination',
+    // dynamicBullets: true, // todo: only PC
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  modules: [Navigation, Pagination],
+};
+
+export const feedbackSwiper = new Swiper(feedbackSwiperElement, swiperOptions);
