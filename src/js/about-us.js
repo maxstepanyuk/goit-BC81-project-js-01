f877bc36f550ad1070d2e5ed9182e7854269cf60
ClementineLavender <natalibulavina78@gmail.com>
2026-07-06 10:16:57 +0200

    everithing is done

--- src/js/about-us.js
+++ src/js/about-us.js
@@ -2,8 +2,6 @@ import Swiper from 'swiper';
 import { Navigation, Pagination } from 'swiper/modules';
 
 import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
 
 const aboutUsSlider = document.querySelector('.about-us-slider');
 
